b0f9de4b851cfba60fc5e53a971e2cf4b988ab65
Rajaneesh <rajaneeshk90@gmail.com>
2024-06-27 15:50:33 +0530

    adding support for rest of the APIs, select init ....

--- controllers/mobility/index.js
+++ controllers/mobility/index.js
@@ -1,5 +1,62 @@
 module.exports = {
     search: (c, req, res) => {
-      res.status(200).json({ message: 'Mobility payload is successfully Validated Against OAS3.1' });
+      res.status(200).json({ message: 'mobility search Payload Validated Against OAS3.1' });
+    },
+    select: (c, req, res) => {
+      res.status(200).json({ message: 'mobility select Payload Validated Against OAS3.1' });
+    },
+    init: (c, req, res) => {
+      res.status(200).json({ message: 'mobility init Payload Validated Against OAS3.1' });
+    },
+    confirm: (c, req, res) => {
+      res.status(200).json({ message: 'mobility confirm Payload Validated Against OAS3.1' });
+    },
+    status: (c, req, res) => {
+      res.status(200).json({ message: 'mobility status Payload Validated Against OAS3.1' });
+    },
+    track: (c, req, res) => {
+      res.status(200).json({ message: 'mobility track Payload Validated Against OAS3.1' });
+    },
+    cancel: (c, req, res) => {
+      res.status(200).json({ message: 'mobility cancel Payload Validated Against OAS3.1' });
+    },
+    update: (c, req, res) => {
+      res.status(200).json({ message: 'mobility update Payload Validated Against OAS3.1' });
+    },
+    rating: (c, req, res) => {
+      res.status(200).json({ message: 'mobility rating Payload Validated Against OAS3.1' });
+    },
+    support: (c, req, res) => {
+      res.status(200).json({ message: 'mobility support Payload Validated Against OAS3.1' });
+    },
+    on_search: (c, req, res) => {
+      res.status(200).json({ message: 'mobility On_search Payload Validated Against OAS3.1' });
+    },
+    on_select: (c, req, res) => {
+      res.status(201).json({ message: 'mobility on_select Payload Validated Against OAS3.1' });
+    },
+    on_init: (c, req, res) => {
+      res.status(200).json({ message: 'mobility on_init Payload Validated Against OAS3.1' });
+    },
+    on_confirm: (c, req, res) => {
+      res.status(200).json({ message: 'mobility on_confirm Payload Validated Against OAS3.1' });
+    },
+    on_status: (c, req, res) => {
+      res.status(200).json({ message: 'mobility on_status Payload Validated Against OAS3.1' });
+    },
+    on_track: (c, req, res) => {
+      res.status(200).json({ message: 'mobility on_track Payload Validated Against OAS3.1' });
+    },
+    on_cancel: (c, req, res) => {
+      res.status(200).json({ message: 'mobility on_cancel Payload Validated Against OAS3.1' });
+    },
+    on_update: (c, req, res) => {
+      res.status(200).json({ message: 'mobility on_update Payload Validated Against OAS3.1' });
+    },
+    on_rating: (c, req, res) => {
+      res.status(200).json({ message: 'mobility on_rating Payload Validated Against OAS3.1' });
+    },
+    on_support: (c, req, res) => {
+      res.status(200).json({ message: 'mobility on_support Payload Validated Against OAS3.1' });
     },
   };
--- controllers/retail/index.js
+++ controllers/retail/index.js
@@ -1,5 +1,62 @@
 module.exports = {
-    search: (c, req, res) => {
-      res.status(200).json({ message: 'Retail payload is successfully Validated Against OAS3.1' });
-    },
-  };
+  search: (c, req, res) => {
+    res.status(200).json({ message: 'retail search Payload Validated Against OAS3.1' });
+  },
+  select: (c, req, res) => {
+    res.status(200).json({ message: 'retail select Payload Validated Against OAS3.1' });
+  },
+  init: (c, req, res) => {
+    res.status(200).json({ message: 'retail init Payload Validated Against OAS3.1' });
+  },
+  confirm: (c, req, res) => {
+    res.status(200).json({ message: 'retail confirm Payload Validated Against OAS3.1' });
+  },
+  status: (c, req, res) => {
+    res.status(200).json({ message: 'retail status Payload Validated Against OAS3.1' });
+  },
+  track: (c, req, res) => {
+    res.status(200).json({ message: 'retail track Payload Validated Against OAS3.1' });
+  },
+  cancel: (c, req, res) => {
+    res.status(200).json({ message: 'retail cancel Payload Validated Against OAS3.1' });
+  },
+  update: (c, req, res) => {
+    res.status(200).json({ message: 'retail update Payload Validated Against OAS3.1' });
+  },
+  rating: (c, req, res) => {
+    res.status(200).json({ message: 'retail rating Payload Validated Against OAS3.1' });
+  },
+  support: (c, req, res) => {
+    res.status(200).json({ message: 'retail support Payload Validated Against OAS3.1' });
+  },
+  on_search: (c, req, res) => {
+    res.status(200).json({ message: 'retail On_search Payload Validated Against OAS3.1' });
+  },
+  on_select: (c, req, res) => {
+    res.status(201).json({ message: 'retail on_select Payload Validated Against OAS3.1' });
+  },
+  on_init: (c, req, res) => {
+    res.status(200).json({ message: 'retail on_init Payload Validated Against OAS3.1' });
+  },
+  on_confirm: (c, req, res) => {
+    res.status(200).json({ message: 'retail on_confirm Payload Validated Against OAS3.1' });
+  },
+  on_status: (c, req, res) => {
+    res.status(200).json({ message: 'retail on_status Payload Validated Against OAS3.1' });
+  },
+  on_track: (c, req, res) => {
+    res.status(200).json({ message: 'retail on_track Payload Validated Against OAS3.1' });
+  },
+  on_cancel: (c, req, res) => {
+    res.status(200).json({ message: 'retail on_cancel Payload Validated Against OAS3.1' });
+  },
+  on_update: (c, req, res) => {
+    res.status(200).json({ message: 'retail on_update Payload Validated Against OAS3.1' });
+  },
+  on_rating: (c, req, res) => {
+    res.status(200).json({ message: 'retail on_rating Payload Validated Against OAS3.1' });
+  },
+  on_support: (c, req, res) => {
+    res.status(200).json({ message: 'retail on_support Payload Validated Against OAS3.1' });
+  },
+};
